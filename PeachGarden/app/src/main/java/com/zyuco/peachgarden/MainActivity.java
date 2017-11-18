554b984a96c5b2ba83ee1b426cd71d99f3ad6e04
Zhu Chu <mystery490815101@gmail.com>
2017-11-18 20:05:21 +0800

    better status bar

--- PeachGarden/app/src/main/java/com/zyuco/peachgarden/MainActivity.java
+++ PeachGarden/app/src/main/java/com/zyuco/peachgarden/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 
@@ -27,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
 
         initList();
         initListeners();
+        setStatusBarColor();
+    }
+
+    private void setStatusBarColor() {
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(0xFFEDEDED);
     }
 
     private void initListeners() {
