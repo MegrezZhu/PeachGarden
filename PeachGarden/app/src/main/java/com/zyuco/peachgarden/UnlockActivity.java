3a0b94973843a01597910faea82e7581470f6043
Zhu Chu <mystery490815101@gmail.com>
2017-11-20 00:56:32 +0800

    status bar color

--- PeachGarden/app/src/main/java/com/zyuco/peachgarden/UnlockActivity.java
+++ PeachGarden/app/src/main/java/com/zyuco/peachgarden/UnlockActivity.java
@@ -6,6 +6,8 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.daimajia.androidanimations.library.Techniques;
@@ -25,6 +27,13 @@ public class UnlockActivity extends AppCompatActivity {
         setContentView(R.layout.activity_unlock);
         initData();
         initListener();
+        setStatusBarColor();
+    }
+
+    private void setStatusBarColor() {
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(0xFFFAFAFA);
     }
 
     private void initData() {
@@ -34,7 +43,7 @@ public class UnlockActivity extends AppCompatActivity {
     }
 
     private void initListener() {
-        unlockCountView = (TextView)findViewById(R.id.tv_unlock_count);
+        unlockCountView = findViewById(R.id.tv_unlock_count);
         unlockCountView.setText(Tools.num2String(unlockCount));
         findViewById(R.id.btn_unlock).setOnClickListener(new View.OnClickListener() {
             @Override
